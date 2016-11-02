fb6d38abcf0b0734c373624024a42aa82ab2cacd
Norman Hernandez <normanhernandez011@gmail.com>
2016-11-02 15:10:31 +0000

    tarea3

---
+++ src/Estrella.java
@@ -0,0 +1,12 @@
+
+public class Estrella extends CuerpoEstelar{
+	
+	int luminosidad;
+	
+	Estrella(int posicion, int masa, int luminosidad)
+	{
+		super(posicion, masa);
+		this.luminosidad = luminosidad;
+	}
+
+}
